18b2c1eb5e82dcf4ef0ff251dce6c125b13174e9
Oskar Radon <oskarradonkimball@gmail.com>
2016-04-05 11:41:35 -0700

    move api key to .env file

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,12 @@
+import config from '../config/environment';
 import Ember from 'ember';
 
 
 export default Ember.Route.extend({
   model() {
-    var urlBills = 'http://congress.api.sunlightfoundation.com/bills?apikey=2a3b1ef2f9f5430fa83affc5c4322794';
-    var urlCommittees = 'http://congress.api.sunlightfoundation.com/committees?apikey=2a3b1ef2f9f5430fa83affc5c4322794';
+    var key = config.myApiKey;
+    var urlBills = 'http://congress.api.sunlightfoundation.com/bills?apikey=' + key;
+    var urlCommittees = 'http://congress.api.sunlightfoundation.com/committees?apikey=' + key;
 
     return Ember.RSVP.hash({
       bills: Ember.$.getJSON(urlBills).then(function(responseJSON){
@@ -12,7 +14,7 @@ export default Ember.Route.extend({
       }),
 
       committees: Ember.$.getJSON(urlCommittees).then(function(responseJSON){
-        return responseJSON.results.slice(0, 5);
+        return responseJSON.results;
       })
     });
   },
