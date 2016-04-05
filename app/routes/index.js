61bc00a869dbf33f7fc567973da17465ec4cb1b6
Oskar Radon <oskarradonkimball@gmail.com>
2016-04-05 10:45:56 -0700

    Change model to multiple API calls

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,12 +2,21 @@ import Ember from 'ember';
 
 
 export default Ember.Route.extend({
-  model: function() {
-    var url = 'http://congress.api.sunlightfoundation.com/bills?apikey=2a3b1ef2f9f5430fa83affc5c4322794';
-    return Ember.$.getJSON(url).then(function(responseJSON){
-      return responseJSON.results.slice(0, 5);
+  model() {
+    var urlBills = 'http://congress.api.sunlightfoundation.com/bills?apikey=2a3b1ef2f9f5430fa83affc5c4322794';
+    var urlCommittees = 'http://congress.api.sunlightfoundation.com/committees?apikey=2a3b1ef2f9f5430fa83affc5c4322794';
+
+    return Ember.RSVP.hash({
+      bills: Ember.$.getJSON(urlBills).then(function(responseJSON){
+        return responseJSON.results.slice(0, 5);
+      }),
+
+      committees: Ember.$.getJSON(urlCommittees).then(function(responseJSON){
+        return responseJSON.results.slice(0, 5);
+      })
     });
   },
+
   actions: {
     zipLookup(params) {
       this.transitionTo('results', params.zip);
